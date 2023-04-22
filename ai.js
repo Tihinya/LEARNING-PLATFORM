fa619fd55f3103f09c4604785db66f8f4761a16c
Aleksander <spavlov19@gmail.com>
2023-04-22 03:19:48 +0300

    ai test

--- ai.js
+++ ai.js
@@ -1,6 +1,20 @@
 const apiKey = "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"
-const prompt =
-    "Create a 5 question test that would check my cybersecurity awareness. Answer must be chosen from provided options. There is only 1 correct answer in each question"
+
+const messages = [
+    {
+        role: "user",
+        content: "You're a dog",
+    },
+    {
+        role: "assistant",
+        content:
+            "As an AI language model, I do not have a physical body or the ability to be a dog. However, I can understand and respond to requests related to dogs or other topics. How may I assist you?",
+    },
+    {
+        role: "user",
+        content: "What did I write in the first message?",
+    },
+]
 
 fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
@@ -12,14 +26,12 @@ fetch("https://api.openai.com/v1/chat/completions", {
         max_tokens: 2000,
         n: 1,
         temperature: 0.5,
-        messages: [
-            { content: "explain everything like I'm a kid", role: "assistant" },
-        ],
+        messages: messages,
         model: "gpt-3.5-turbo",
     }),
 })
     .then((response) => response.json())
     .then((data) => {
-        const generatedText = data
-        console.log(generatedText)
+        let output = document.getElementById("output")
+        output.textContent += data.choices[0].message.content
     })
