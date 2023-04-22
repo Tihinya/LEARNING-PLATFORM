a643f558d4ddeef09939689f029b6926d5681bf5
Aleksander <spavlov19@gmail.com>
2023-04-22 16:16:08 +0300

    start test

--- ai/ai.js
+++ ai/ai.js
@@ -1,13 +1,13 @@
 const apiKey = "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"
 let score = 5
-function handlePrompt(newMessage, messageHistory = [], callback) {
+async function handlePrompt(newMessage, messageHistory = [], callback) {
     if (newMessage === "") {
         return
     }
     // display user message in chat output
     messageHistory.push({ role: "user", content: newMessage })
     // generate response from chatbot
-    fetch("https://api.openai.com/v1/chat/completions", {
+    return fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -31,15 +31,15 @@ function handlePrompt(newMessage, messageHistory = [], callback) {
 
 const summary = (data) => {
     const botMessage = data.choices[0].message.content
-    const correctReg = /^Correct/
-    const wrongReg = /^Wrong/
+    const correctReg = /^True/
+    const wrongReg = /^False/
     if (correctReg.test(botMessage)) {
         score++
     } else if (wrongReg.test(botMessage)) {
         score--
     }
-    const chatOutput = document.getElementById("chat-output")
-    chatOutput.innerHTML += `<div><strong>Chatbot:</strong> ${botMessage}</div>`
+    console.log(score)
+    return botMessage
 }
 
 function testPrompt() {
@@ -49,6 +49,15 @@ function testPrompt() {
             content: "Save my score: my score is " + score + " points",
         },
     ]
+    const startBtn = document.getElementById("start-btn")
+    startBtn.addEventListener("click", () => {
+        const startingPrompt =
+            'Write it without saying "Sure........" , i need only question and options and without printing  scale. I want you to act as a cybersecurity assistant. Using your experience in cybersecurity, create a question with an answer choice about Phishing. Make the question no longer than 50 words. On a scale from 1 to 10 I understand this topic on 5. Do not write an answer. Write only question and options'
+
+        let botMessage = handlePrompt(startingPrompt, messageHistory, summary)
+        // const chatOutput = document.getElementById("chat-output")
+        // // chatOutput.innerHTML += `<div><strong>Chatbot:</strong> ${botMessage}</div>`
+    })
 
     const sendBtn = document.getElementById("send-btn")
     sendBtn.addEventListener("click", () => {
@@ -62,4 +71,4 @@ function testPrompt() {
 
 testPrompt()
 
-export { handlePrompt, summary }
+export { handlePrompt }
