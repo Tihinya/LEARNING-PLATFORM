9c77cd76a71b2f42af446bd8e7c6817827f7c5b5
mercey123 <erik1telpt2@gmail.com>
2023-04-22 12:10:50 +0300

    SEC-5 added static file serving

--- server/server.mjs
+++ server/server.mjs
@@ -1,24 +1,25 @@
 import express from "express"
 import cors from "cors"
 import path from "path"
-import { fileURLToPath } from "url"
-import { dirname } from "path"
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
+import cfg from "./config.mjs"
 const app = express()
-const port = 3000
-app.use(express.json())
+
 app.use(
     cors({
         origin: "*",
         methods: "*",
     })
 )
+app.use(express.json())
+app.use(express.static(path.join(cfg.__dirname, "..", "src")))
 
 //get request to '/'
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/test.html"))
+    res.sendFile(path.join(cfg.__dirname, "..", "src/index.html"))
+})
+
+app.get("/materials", (req, res) => {
+    res.sendFile(path.join(cfg.__dirname, "..", "src/materials.html"))
 })
 
 //post request to '/'
@@ -32,6 +33,6 @@ app.post("/", (req, res) => {
     }
 })
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
+app.listen(cfg.port, () => {
+    console.log(`Server running at http://localhost:${cfg.port}`)
 })
